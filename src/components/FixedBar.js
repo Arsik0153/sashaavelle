f698f91b09aa698dde083eed075034b12f453ee7
Жанарыс Сайранбеков <zhanaryssairanbekov@zhans.dlink>
2020-09-25 20:23:46 +0600

    fonts add

---
+++ src/components/FixedBar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import inst from './../assets/images/instagram.svg'
+import drib from './../assets/images/dribbble.svg'
+
+const FixedBar = () => {
+  return (
+    <div className="container fixedBar-container">
+      <div className="top">
+        <h3>Sasha Avelle</h3>
+        <h3>Contact me</h3>
+      </div>
+    </div>
+  )
+}
+
+export default FixedBar
